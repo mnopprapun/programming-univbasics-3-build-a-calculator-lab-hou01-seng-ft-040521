932ee519a888e6383e8bbdf211e0ef059a9856bb
Marc Nopprapun <mnopprapun@gmail.com>
2021-03-09 19:27:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
+require 'math_spec.rb'
 def addition(num1, num2)
-
+  addition=num1+num2
 end
 
 def subtraction(num1, num2)
-
-end
-
-def division(num1, num2)
-
+ subtraction=num1-num2
 end
 
 def multiplication(num1, num2)
+multiplication=num1*num2
+end
 
+def division (num1, num2)
+  division=num1/num2
 end
 
 def modulo(num1, num2)
-
+  modulo=num1%num2
 end
 
-def square_root(num)
-
+def square_root(num1, num2)
+ square_root = math.sqrt(num1)
+ square_root = math.sqrt(num2)
 end
